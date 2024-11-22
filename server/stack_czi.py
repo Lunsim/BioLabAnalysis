f68718c69374a32258127856ce0ffe97ab1f6a3a
qiucd2025 <143422403+qiucd2025@users.noreply.github.com>
2024-11-22 15:48:18 -0500

    Update stack_czi.py

--- server/stack_czi.py
+++ server/stack_czi.py
@@ -25,11 +25,51 @@ import statistics as stats
 
 micron_to_pixel_scale = 3.0769
 
-def stack_czi(images):
-    image_list = []
-    for img in images:
-        img = AICSImage(file_path)
-        image_data = img.get_image_data("YX")
-        #uint8_stacked = im_adjust(image_data, autoscale = 'uint8')
-        image_list.append(image_data)
-    return image_list
+def im_adjust(I, thres=[1, 99, True], autoscale=None):
+    # compute percentile: remove too big or too small values
+    # thres: [thres_low, thres_high, percentile]
+    if thres[2]:
+        I_low, I_high = np.percentile(I.reshape(-1), thres[:2])
+    else:
+        I_low, I_high = thres[0], thres[1]
+    # thresholding
+    I[I > I_high] = I_high
+    I[I < I_low] = I_low
+    if autoscale is not None:
+        # scale to 0-1
+        I = (I.astype(float) - I_low) / (I_high - I_low)
+        if autoscale == "uint8":
+            # convert it to uint8
+            I = (I * 255).astype(np.uint8)
+    return I
+
+def stack_czi(image_path, storage_path):
+    filename = os.listdir(image_path)
+    file_names = []
+    sorted_filenames = sorted(filename, key=lambda x: (int(x.split()[1].split('.')[0]), int(x.split()[1].split('.')[1])))
+    a_animal = [0]
+    for i in range(1, len(sorted_filenames)):
+        prev_animal = int(sorted_filenames[i-1].split()[1].split('.')[0])
+        curr_animal = int(sorted_filenames[i].split()[1].split('.')[0])
+        if prev_animal != curr_animal:
+            a_animal.append(i * 2)
+
+    for filename in sorted_filenames:
+        if filename.endswith('.czi'):
+            print(filename)
+            file_path = os.path.join(folder_path, filename)
+            img = AICSImage(file_path)
+            channel_img = []
+            for c in range(img.shape[1]):
+              image = img.get_image_data("ZYX", C=c)
+              image = np.max(image, axis = 0)
+              channel_img.append(cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE))
+            stacked_image = np.stack(channel_img, axis=-1)
+            uint8_stacked = im_adjust(stacked_image, autoscale = 'uint8')
+
+            base_filename = os.path.splitext(filename)[0]
+
+            tiff_file_path = os.path.join(storage_path, f'{base_filename}.tiff')
+            file_names.append(tiff_file_path)
+            tifffile.imwrite(tiff_file_path, uint8_stacked)
+    return file_names
